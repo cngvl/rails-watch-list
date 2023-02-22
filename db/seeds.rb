03ee4fd5da37b972be9fa09687d423097e05d07d
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-02-22 14:44:37 +1100

    finished with list index and show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Movie.destroy_all
+List.destroy_all
+
+
+5.times do
+  p movie_name = Faker::Movie.unique.title
+  Movie.create!(
+    title: movie_name,
+    overview: Faker::Movie.quote,
+    poster_url: "https://unsplash.com/s/photos/movie/#{movie_name}",
+    rating: rand(1..10)
+  )
+  List.create!(
+    name: Faker::Lorem.sentence(word_count: 3)
+  )
+end
