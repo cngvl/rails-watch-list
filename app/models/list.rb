d7f7ca4857aa9db0d35ad874c2662d78bb311e21
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-02-24 11:17:32 +1100

    added bootstrap tags

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,7 +5,6 @@ class List < ApplicationRecord
   has_one_attached :photo
 end
 
-
 # A movie must have a unique title and an overview.
 # A list must have a unique name.
 # A bookmark must be linked to a movie and a list, and the [movie, list] pairings should be unique.
